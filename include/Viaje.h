49634e444aa375f942e9d28f28696ce20aa78afb
Felipe Putrelli <feli1712@hotmail.com>
2026-05-27 10:47:46 -0300

    Viaje.h WIP

--- include/Viaje.h
+++ include/Viaje.h
@@ -2,7 +2,13 @@
 #define VIAJE_H
 
 #include "DTFecha.h"
+#include "DTConsultaViaje.h"
+#include "DTListarViaje.h"
+#include "DTUsuarioViaje.h"
+#include "Pasajero.h"
+#include "Reserva.h"
 #include <string>
+#include <vector>
 
 class Viaje {
 private:
@@ -12,10 +18,39 @@ private:
     std::string destino;
     int asientosPublicados;
     float precio;
-
+    std::vector<Reserva> reservas;
+    
 public:
     Viaje(int codigo, DTFecha fecha, std::string origen, std::string destino, int asientosPublicados, float precio);
     ~Viaje();
+
+    //Getters
+    int getAsientosPublicados();
+    int getCodigo();
+    DTFecha getFecha();
+    std::string getOrigen();
+    std::string getDestino();
+
+    //consultarViajes
+    bool cumpleDatos(DTFecha fecha, std::string origen, std::string destino);
+    bool consultarAsientos(int asientos);
+    DTConsultaViaje crearDTConsultaViaje();
+
+    //generarReserva
+    bool relacion(Pasajero p);
+    int totalAsientosRes();
+    void crearReserva(int asientos, DTFecha fecha);
+
+    //listarViajes
+    std::vector<DTListarViaje> crearDTViajes(Pasajero p);       //DCD: Usuario u en vez de Pasajero p (así ahorro un include)
+
+    //listarUsuariosViaje
+    std::vector<DTUsuarioViaje> obtenerPasajeros();
+    DTUsuarioViaje obtenerConductor();
+
+    //calificarUsuario
+    Reserva obtenerReservaCalif();
+    bool coincideCalif(Usuario u, Usuario u_calif);             //DCD: coincide en vez de coincideCalif
 };
 
 #endif
--- src/Viaje.cpp
+++ src/Viaje.cpp
@@ -9,4 +9,48 @@ Viaje::Viaje(int codigo, DTFecha fecha, std::string origen, std::string destino,
     this->precio = precio;
 }
 
-Viaje::~Viaje() {}
+Viaje::~Viaje() {};
+
+float Viaje::getPrecio() const { return this->precio; }
+
+int Viaje::getAsientosPublicados() const { return this->asientosPublicados; }
+
+int Viaje::getCodigo() const { return this->codigo; }
+
+DTFecha Viaje::getFecha() const { return this->fecha; }
+
+std::string Viaje::getOrigen() const { return this->origen; }
+
+std::string Viaje::getDestino() const { return this->destino; }
+
+bool Viaje::cumpleDatos(DTFecha fecha, std::string origen, std::string destino)
+{
+    bool cond1 = fecha == this->fecha;
+    bool cond2 = origen == this->origen;
+    bool cond3 = destino == this->destino;
+
+    return cond1 && cond2 && cond3;
+}
+
+bool Viaje::consultarAsientos(int asientos)
+{
+    int reservados = 0;
+    for (const auto& r : reservas)
+        reservados = reservados + r.getAsientosReservados();
+    
+    return (reservados + asientos) <= this->asientosPublicados;
+}
+
+DTConsultaViaje Viaje::crearDTConsultaViaje()
+{
+
+    int codigo = this->getCodigo();
+    std::string marca = (this->veh).getMarca();
+    std::string modelo = (this->veh).getModelo();
+    std::string conductor = (this->veh).obtenerConductor();
+    float calif = (this->veh).obtenerCalifConductor();
+    float precio = this->getPrecio();
+
+    DTConsultaViaje res = DTConsultaViaje(codigo, marca, modelo, conductor, calif, precio);
+    return res;
+}

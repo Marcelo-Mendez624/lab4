cf6ea3aacc9b047f3d86e5284c5bd3432a45768e
sebastianruchansky <sebastianruchansky@gmail.com>
2026-05-27 09:33:39 -0300

    yiiyiggyig

--- include/Vehiculo.h
+++ include/Vehiculo.h
@@ -15,6 +15,18 @@ private:
 public:
     Vehiculo(std::string matricula, int capacidad, std::string marca, std::string modelo, TipoVehiculo tipo);
     ~Vehiculo();
+    
+    //getters
+    std::string getMatricula();
+    std::string getMarca();
+    TipoVehiculo getTipo();
+    int getCapacidad();
+
+    
+    std::string getConductor();
+    float getCalifConductor();
+    DTUsuarioViaje getNickConductor()
+    std::vector<DTListarViajes> listarViajes(); 
 };
 
 #endif

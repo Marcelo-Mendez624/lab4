b36d8de1fffe271b27bb5688b87032331dd26d7d
sebastianruchansky <sebastianruchansky@gmail.com>
2026-05-27 10:50:46 -0300

    Update Calificacion.h

--- include/Calificacion.h
+++ include/Calificacion.h
@@ -7,12 +7,16 @@ class Calificacion {
 private:
     DTFecha fecha;
     int puntaje;
-
+    std set<Usuario> usuariosCalificados; // Conjunto para almacenar los usuarios que han sido calificados
+    std set<Reserva> reservasCalificadas; // Conjunto para almacenar las reservas que han sido calificadas
 public:
     Calificacion(DTFecha fecha, int puntaje);
     ~Calificacion();
 
     int getPuntaje() const { return puntaje; }
+    bool coicidenUsuario(u: Usuario, u_Calif: usuario);
+    void linkerCalifUsuario(u: Usuario, u_Calif: usuario);
 };
 
 #endif
+

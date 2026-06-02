d27603d824fc76cb1cf778a73882c98b7d7a6860
Marcelo Mendez <83145704+Marcelo-Mendez624@users.noreply.github.com>
2026-06-02 09:39:27 -0300

    Interface alta usuario

--- include/IAltaUsuario.h
+++ include/IAltaUsuario.h
@@ -1,10 +1,17 @@
-#ifndef IControladorUsuario_H
-#define IControladorUsuario_H
+#ifndef IAltaUsuario_H
+#define IAltaUsuario_H
 
-class IControladorUsuario
+#include <string>
+#include <vector>
+#include "Tipolibreta.h"
+#include "TipoVehiculo.h"
+
+class IAltaUsuario
 {
 public:
-  virtual void altaUsuario() = 0;
+  virtual bool AltaPasajero(std::string nombre, std::string contrasena, std::string email, std::string ci) = 0;
+  virtual bool AltaConductor(std::string nombre, std::string contrasena, std::string email, std::vector<TipoLibreta> libretas) = 0;
+  virtual int registrarVehiculo(std::string nickname, std::string matricula, int capacidad, std::string marca, TipoVehiculo tipo) = 0;
 };
 
 #endif

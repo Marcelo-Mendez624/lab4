0a2f64f82002087705f29bd94204f0774d582cfb
Marcelo Mendez <83145704+Marcelo-Mendez624@users.noreply.github.com>
2026-06-02 09:45:28 -0300

    Cambio elminar viaje

--- include/IEliminarViaje.h
+++ include/IEliminarViaje.h
@@ -11,8 +11,8 @@ public:
 
   virtual std::list<std::string> listarViajes() = 0;
   virtual DTDetalleViaje DetalleViaje(int codigo) = 0;
-  virtual bool eliminarViaje (int codigo) = 0;
-  virtual void cancelarEliminarViaje(int codigo) = 0;
+  virtual bool eliminarViaje () = 0;
+  virtual void cancelarEliminarViaje() = 0;
 
 };
 #endif

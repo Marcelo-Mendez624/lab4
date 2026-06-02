3d5f7f01b9054611f883182b83f3ece9a34ec0f4
Marcelo Mendez <83145704+Marcelo-Mendez624@users.noreply.github.com>
2026-06-02 09:05:32 -0300

    IControladorViaje

--- include/IControladorReserva.h
+++ include/IControladorReserva.h
@@ -8,8 +8,10 @@
 class IControladorReserva
 {
 public:
+
   virtual std::list<std::string> listarUsuarios()=0;
-  virtual std::set<DTConsultaViaje> consultarViajes(DTFecha fecha,std::string origen,std::string destino,int asientos) =0;
+  virtual std::set<DTConsultaViaje> consultarViajes(DTFecha fecha,std::string origen,std::string destino,int asientos) = 0;
   virtual bool generarReserva (std::string nickname,int codigo,int asientos) = 0;
+
 };
 #endif
---
+++ include/IControladorViaje.h
@@ -0,0 +1,10 @@
+#ifndef ICONTROLADORVIAJE_H
+#define ICONTROLADORVIAJE_H
+
+class IControladorViaje
+{
+public:
+  virtual void altaViaje() = 0;
+};
+
+#endif 

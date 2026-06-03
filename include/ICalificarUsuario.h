0984ce8adb46df3d8e00a6fe193ac716320dbf35
sebastianruchansky <sebastianruchansky@gmail.com>
2026-06-03 12:58:41 -0300

    Update ICalificarUsuario.h

--- include/ICalificarUsuario.h
+++ include/ICalificarUsuario.h
@@ -11,7 +11,7 @@ class ICalificarUsuario
 {
 public:
   virtual std::map<std::string, DTUsuario> listarUsuarios() = 0;
-  virtual std::map<std::string, DTConsultaViaje> listarViajes(std::string nickname) = 0;
+  virtual std::vector<DTListarViaje> listarViajes(std::string nickname) = 0;
   virtual std::map<std::string, DTUsuarioViaje> listarUsuariosViaje(int codigoViaje) = 0;
   virtual bool calificarUsuario(std::string nickname, int calificacion) = 0;
 };

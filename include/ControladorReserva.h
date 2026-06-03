0bd1c508d0a92a5551bc1f377e7aca71a6b178e1
sebastianruchansky <sebastianruchansky@gmail.com>
2026-06-03 14:06:42 -0300

    Update ControladorReserva.h

--- include/ControladorReserva.h
+++ include/ControladorReserva.h
@@ -1,1 +1,24 @@
+#ifndef CONTROlADORRESERVA_H
+#define CONTROlADORRESERVA_H
 
+#include "IGenerarReserva.h"
+#include "DTConsultaViaje.h"
+#include <string>
+#include <list>
+#include <set>
+
+
+class ControladorReserva : public IGenerarReserva 
+{
+  private:
+    static ControladorReserva *instancia;
+    ControladorReserva();
+
+  public:
+    static ControladorReserva *getInstancia();
+    std::list<std::string> listarUsuarios() ;
+    std::set<DTConsultaViaje> consultarViajes(DTFecha fecha,std::string origen,std::string destino,int asientos);
+    bool generarReserva (std::string nickname,int codigo,int asientos) ;
+};
+
+#endif

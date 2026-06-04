cff901fe997c193df0936c5459276090bb5caad2
Marcelo Mendez <83145704+Marcelo-Mendez624@users.noreply.github.com>
2026-06-04 09:31:06 -0300

    cosa

--- src/ControladorUsuario.cpp
+++ src/ControladorUsuario.cpp
@@ -1,4 +1,7 @@
 #include "../include/ControladorUsuario.h"
+#include "../include/ManejadorUsuario.h"
+#include "../include/Conductor.h"
+
 ControladorUsuario::ControladorUsuario() 
 {
     
@@ -11,18 +14,43 @@ ControladorUsuario::~ControladorUsuario()
 
 bool ControladorUsuario::AltaPasajero(std::string nombre, std::string contrasena, std::string email, std::string ci) 
 {
-    // Implementación de la lógica para dar de alta a un pasajero
+    ManejadorUsuario* manejador = ManejadorUsuario::getInstance();
+    
+    if(manejador->existeUsuario(nombre)) 
+    {
+        return false; 
+    }
+
+    manejador->nuevoPasajero(nombre, nombre, contrasena, email, ci);
+
     return true; // Retorna true si el alta fue exitosa, false en caso contrario
 }
 
 bool ControladorUsuario::AltaConductor(std::string nombre, std::string contrasena, std::string email, std::vector<TipoLibreta> libretas) 
 {
-    // Implementación de la lógica para dar de alta a un conductor
+    ManejadorUsuario* manejador = ManejadorUsuario::getInstance();
+    if(manejador->existeUsuario(nombre)) 
+    {
+        return false; 
+    }
+
+    manejador->nuevoConductor(nombre, nombre, contrasena, email, std::set<TipoLibreta>(libretas.begin(), libretas.end()));
     return true; // Retorna true si el alta fue exitosa, false en caso contrario
 }
 
 int ControladorUsuario::registrarVehiculo(std::string nickname, std::string matricula, int capacidad, std::string marca, TipoVehiculo tipo) 
 {
-    // Implementación de la lógica para registrar un vehículo
+    ManejadorUsuario* manejador = ManejadorUsuario::getInstance();
+    
+        class Conductor* conductor = dynamic_cast<class Conductor*>(manejador->obtenerUsuario(nickname));
+
+        /*
+        if(conductor.tieneLibreta(TipoVehiculo))
+        {
+
+        }
+        */
+        
+        // Aquí se debería agregar la lógica para registrar el vehículo al conductor
     return 0; // Retorna un identificador único para el vehículo registrado
 }

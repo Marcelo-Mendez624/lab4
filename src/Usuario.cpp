bef541dbe8d84fe28f00989961f24de619cec820
sebastianruchansky <sebastianruchansky@gmail.com>
2026-06-04 11:15:43 -0300

    Update Usuario.cpp

--- src/Usuario.cpp
+++ src/Usuario.cpp
@@ -32,7 +32,25 @@ float Usuario::getCalificacionPromedio() const
     return calificacionTotal / calificaciones.size();
 }
 
-std::vector<DTListarViaje> Usuario::getViajes() const
-{
-    return viajes; //FALTA IMPLEMENTAR LA LOGICA, IMPORTANTE
+std::vector<DTListarViaje> Usuario::getViajes() const {
+    std::vector<DTListarViaje> viajes;
+    
+    const class Conductor* conductor = dynamic_cast<const class Conductor*>(this);
+
+    if (conductor != nullptr) {
+        
+        for (Vehiculo* v : conductor->getVehiculos()) {
+            std::vector<DTListarViaje> listarViajes = v->listarViajes();
+            viajes.insert(viajes.end(), listarViajes.begin(), listarViajes.end());
+        }
+    } else {
+        const class Pasajero* pasajero = dynamic_cast<const class Pasajero*>(this);
+        ManejadorViaje* mv = ManejadorViaje::getInstance();
+        std::map<int, Viaje*> setvi = mv->getViajes();
+        for (const auto& vi : setvi) {
+            std::vector<DTListarViaje> crearDTViajes = vi.second->crearDTViajes(pasajero);
+            viajes.insert(viajes.end(), crearDTViajes.begin(), crearDTViajes.end());
+        } 
+    }
+    return viajes;
 }
